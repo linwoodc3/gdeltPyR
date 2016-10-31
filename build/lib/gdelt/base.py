8a5e4d8da955d998dc7588ad8de93a3551ee0b8f
linwoodc3 <valinvescap@gmail.com>
2016-10-31 13:39:22 -0400

    adding files to correct the file download

--- build/lib/gdelt/base.py
+++ build/lib/gdelt/base.py
@@ -3,6 +3,7 @@ from functools import partial
 from multiprocessing import Pool, cpu_count
 
 import pandas as pd
+import requests
 import json
 import os
 
@@ -22,7 +23,16 @@ from gdelt.helpers import cameos
 this_dir, this_filename = os.path.split(__file__)
 BASE_DIR = os.path.dirname(this_dir)
 UTIL_FILES_PATH = os.path.join(BASE_DIR, "utils","schema_csvs")
-codes = json.load(open(os.path.join(UTIL_FILES_PATH,"cameoCodes.json")))
+
+a = 'https://raw.githubusercontent.com/linwoodc3/gdeltPyR/master/' \
+    'utils/schema_csvs/cameoCodes.json'
+
+
+codes = json.loads((requests.get(a).content.decode('utf-8')))
+
+
+
+
 
 
 

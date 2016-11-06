efedbf8e12326bbd638845116a9becb320ee7535
linwoodc3 <valinvescap@gmail.com>
2016-11-06 04:48:38 -0500

    removing pypi distro files from github

--- build/lib/gdelt/helpers.py
+++
@@ -1,9 +0,0 @@
-##############################
-# Filter functions for dataframes
-##############################
-
-def cameos(x,codes):
-    try:
-        return codes['Description'][x]
-    except:
-        return ("No Description returned for CAMEO code {0}".format(x))
